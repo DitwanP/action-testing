6ea0e3775637c7a32406e130377b2c411ae21b79
DitwanP <price.ditwan@gmail.com>
2024-12-04 13:36:24 -0800

    cleanup

--- .github/scripts/omitPrFromChangelog.js
+++ .github/scripts/omitPrFromChangelog.js
@@ -1,6 +1,6 @@
 // @ts-check
 const {
-  labels: { handoff, planning },
+  labels: { planning },
 } = require("./support/resources");
 
 /** @param {import('github-script').AsyncFunctionArguments} AsyncFunctionArguments */
