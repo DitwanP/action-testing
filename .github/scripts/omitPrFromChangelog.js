f63159d7c4a485e49700655ec0ccc8817661511f
DitwanP <price.ditwan@gmail.com>
2024-12-09 10:16:31 -0800

    update action to not duplicate omit comments

--- .github/scripts/omitPrFromChangelog.js
+++ .github/scripts/omitPrFromChangelog.js
@@ -7,24 +7,33 @@ const {
 module.exports = async ({ github, context }) => {
   const { repo, owner } = context.repo;
   const payload = /** @type {import('@octokit/webhooks-types').PullRequestLabeledEvent} */ (context.payload);
-  const {label, pull_request: { number },} = payload;
+  const {
+    label,
+    pull_request: { number },
+  } = payload;
 
   const pullRequestBody = payload.pull_request.body;
-  const ommitComment = `\n\nBEGIN_COMMIT_OVERRIDE\nEND_COMMIT_OVERRIDE`
+  const omitComment = `\n\nBEGIN_COMMIT_OVERRIDE\nEND_COMMIT_OVERRIDE`;
+  const omitCommentRegex = /BEGIN_COMMIT_OVERRIDE\nEND_COMMIT_OVERRIDE/gm;
 
   if (!pullRequestBody) {
-    console.log("No issue body was found");
+    console.log("No issue body was found, ending run.");
     return;
   }
 
-   if (label?.name === planning.noChangelogEntry) {
+  if (pullRequestBody.match(omitCommentRegex)) {
+    console.log("This PR is already omitted from the changelog, ending run.");
+    return;
+  }
+
+  if (label?.name === planning.noChangelogEntry) {
     const pullRequestProps = {
       owner,
       repo,
       pull_number: number,
     };
 
-    const newPullRequestBody = pullRequestBody + ommitComment;
+    const newPullRequestBody = `${pullRequestBody}${omitComment}`;
 
     await github.rest.pulls.update({
       ...pullRequestProps,
