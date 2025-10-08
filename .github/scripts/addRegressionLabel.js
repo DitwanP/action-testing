5596876395dd90ff7239c16deed1d34a39212f8c
DitwanP <price.ditwan@gmail.com>
2025-10-08 11:14:54 -0700

    update regression label action

--- .github/scripts/addRegressionLabel.js
+++ .github/scripts/addRegressionLabel.js
@@ -17,6 +17,7 @@ module.exports = async ({ github, context }) => {
     return;
   }
 
+  // If bug label is not present, skip adding regression label.
   if (!labels?.some((label) => label.name === "bug")) {
     console.log("Issue does not have the 'bug' label, skipping regression label addition.");
     return;
@@ -24,18 +25,28 @@ module.exports = async ({ github, context }) => {
 
   const regressionRegex = /(?<=### Regression\?[\r\n|\r|\n]{2}).+$/m;
   const regressionRegexMatch = body.match(regressionRegex);
+  const regressionResponse = (regressionRegexMatch?.[0] || "").trim();
 
-  // If issue has "_No response_" under the regression section then log and exit, otherwise add regression label.
-    const regressionVersion = (regressionRegexMatch?.[0] || "").trim();
+  if (regressionResponse === "_No response_" || regressionResponse === "") {
+    console.log("No regression version provided, not adding regression label.");
+    return;
+  }
 
-    if (regressionVersion === "_No response_" || regressionVersion === "") {
-      console.log("No regression version provided, not adding regression label.");
-    } else {
-      await github.rest.issues.addLabels({
-        issue_number,
-        owner,
-        repo,
-        labels: [bug.regression],
-      });
-    }
+  // Match x.y.z with an optional prerelease extension of "-next.N".
+  // Example matches: 1.2.3, v1.2.3, 3.4.0-next.13
+  const semVerRegex = /\bv?(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)\.(?:0|[1-9]\d*)(?:-next\.(?:0|[1-9]\d*))?\b/i;
+
+  const containsValidVersion = semVerRegex.test(regressionResponse);
+  if (!containsValidVersion) {
+    console.log("No valid version (x.y.z or x.y.z-next.N) found, not adding regression label.");
+    return;
+  }
+
+  // Found a version; add the regression label.
+  await github.rest.issues.addLabels({
+    issue_number,
+    owner,
+    repo,
+    labels: [bug.regression],
+  });
 };
