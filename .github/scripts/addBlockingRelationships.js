ffce4bc1e5625af7effa4c3011243ae34d9397eb
DitwanP <price.ditwan@gmail.com>
2026-04-02 14:13:28 -0700

    misc

--- .github/scripts/addBlockingRelationships.js
+++ .github/scripts/addBlockingRelationships.js
@@ -24,6 +24,7 @@ module.exports = async ({ github, context, core }) => {
 
   const blockedIssuesLineMatch = body.match(blockedIssuesLineRegex);
   if (!blockedIssuesLineMatch) {
+    console.log("No blocked issues line found in the issue description.");
     return;
   }
 
